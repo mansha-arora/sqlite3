a030c0bf22ef0d2da7d9781164ac2f1c093e8ae3
Kheradm <32846834+Kheradm@users.noreply.github.com>
2018-12-13 00:10:54 -0500

    Add files via upload

---
+++ sqlite3_demo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 23:39:14 2018
+
+@author: M
+"""
+
+import sqlite3
+conn = sqlite3.connect('employee.db')
+c=conn.cursor()
+#c.execute("""CREATE TABLE employees (
+#            first text,
+#            last text,
+#            pay integer
+#            )""")
+#conn.commit()
+c.execute("INSERT INTO employees VALUES ('Corey','Schafer',50000)")
+c.execute("SELECT * FROM employees WHERE last='Schafer'")
+print(c.fetchone())
+conn.commit()
+
+conn.close()
